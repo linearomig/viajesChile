b6f5e625138137badfe08d5df24d6cfc091abb63
Aline Romig <linearomig@gmail.com>
2021-11-04 17:05:01 -0300

    Animaciones con JS

---
+++ assets/js/script.js
@@ -0,0 +1,22 @@
+$(function(){
+    // Smooth Scrolling a los links
+    $("a").click(function(event) {
+      if (this.hash !== "") {
+        event.preventDefault();
+  
+        var hash = this.hash;
+  
+        $('html, body').animate({
+          scrollTop: $(hash).offset().top
+        }, 800, function(){
+  
+          window.location.hash = hash;
+        });
+      } 
+    });
+
+    //Añadir tooltip con nombre de las RRSS en los íconos
+    $('[data-toggle="tooltip"]').tooltip()
+
+  });
+
